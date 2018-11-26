5197302a47c6cafd5e885a302ffdf5d66fa53b73
zzy <554084934@qq.com>
2018-11-26 23:10:04 +0800

    习题2

---
+++ src/ch3/p2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+ptr_node init_lots(int* lots, int size);
+void print_lots(ptr_node list, ptr_node p);
+
+int main(void) {
+    int ints[] = {1, 4, 5, 8, 10};
+    ptr_node list = init_common_list(10);
+    ptr_node lots = init_lots(ints, 5);
+    
+    print_lots(list, lots);
+    
+    free(list);
+    free(lots);
+    
+    return 0;
+}
+
+ptr_node init_lots(int* lots, int size) {
+    ptr_node head = malloc(sizeof (struct Node)), p = head;
+    int i;
+    
+    head->next = NULL;
+    for (i = 0; i < size; i++) {
+        p->next = malloc(sizeof (struct Node));
+        p = p->next;
+        p->val = lots[i];
+        p->next = NULL;
+    }
+    
+    return head;
+}
+
+void print_lots(ptr_node list, ptr_node p) {
+    int idx = 0;
+    for (p = p->next; p != NULL; p = p->next) {
+        for (list = list->next; list != NULL; list = list->next) {
+            if (++idx == p->val) {
+                printf("%d ", list->val);
+                break;
+            }
+        }
+    }
+}
